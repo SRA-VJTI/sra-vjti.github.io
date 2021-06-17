0119221609d42216c1fa83dad26b60ac60acf012
Spnetic-5 <saurabhpowar1823@gmail.com>
2021-06-17 18:25:48 +0530

    added github in social icons

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -5,8 +5,11 @@ import {
   faTwitter,
   faInstagram,
   faYoutube,
+  faGithub,
+  faLinkedinIn
 } from '@fortawesome/free-brands-svg-icons';
 import { SocialMediaLinks } from '../../data';
+import { faCode } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
   return (
@@ -14,6 +17,12 @@ const Footer = () => {
       <footer className={styles.footer}>
         <h3>Connect with us</h3>
         <div className={styles.footerIcons}>
+        <a href={SocialMediaLinks.github} target='_blank'>
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a href={SocialMediaLinks.linkedin} target='_blank'>
+            <FontAwesomeIcon icon={faLinkedinIn} />
+          </a>
           <a href={SocialMediaLinks.facebook} target='_blank'>
             <FontAwesomeIcon icon={faFacebook} />
           </a>
--- data/socialmedia.js
+++ data/socialmedia.js
@@ -1,4 +1,6 @@
 const SocialMediaLinks = {
+  github: 'https://github.com/SRA-VJTI',
+  linkedin: 'https://www.linkedin.com/company/sra-vjti/mycompany/',
   facebook: 'https://www.facebook.com/sra.vjti/',
   youtube: 'https://www.youtube.com/channel/UC-g67CbV2Iu30n7TvipRiCA',
   twitter: 'https://twitter.com/sravjti',
