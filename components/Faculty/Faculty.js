4793f3f8e3d531030485bb2a61f16f84739acf34
Spnetic-5 <saurabhpowar1823@gmail.com>
2021-06-21 17:08:19 +0530

    added faculty LinkedIn links

---
+++ components/Faculty/Faculty.js
@@ -0,0 +1,25 @@
+import styles from './Faculty.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+
+const faculty = ({ index, imgName, name, sub, linkedInLink}) => {
+  return (
+    <div className={index % 2 == 0 ? styles.faculty : styles.facultyr}>
+      <div
+        style={{ backgroundImage: `url("/static/images/${imgName}")` }}
+        className={styles.facultyImg}
+      ></div>
+      <div className={styles.facultyCont}>
+        <div className={styles.facultyContName}>{name}</div>
+        <div className={styles.facultyContSub}>{sub}</div>
+        <div className={styles.links}>
+          <a href={linkedInLink}>
+            <FontAwesomeIcon icon={faLinkedinIn} />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default faculty;
--- data/faculty.js
+++ data/faculty.js
@@ -3,22 +3,19 @@ const FacultyInchargeList = [
     name: 'Dr. A. S. Rao',
     imgName: 'Dr. A. S. Rao.jpg',
     sub: <>Faculty Coordinator</>,
-    linkedInLink: '',
-    githubLink: '',
+    linkedInLink: 'https://www.linkedin.com/in/dr-a-s-rao-a82aa620?originalSubdomain=in',
   },
   {
     name: 'Dr. V. B. Suryawanshi',
     imgName: 'Dr. V. B. Suryawanshi.jpg',
     sub: <>Vice-Chairman</>,
-    linkedInLink: '',
-    githubLink: '',
+    linkedInLink: 'https://www.linkedin.com/in/dr-vinod-suryawanshi-b02a8515/?originalSubdomain=in',
   },
   {
     name: 'Dr. S. A. Mastud',
     imgName: 'Dr. S. A. Mastud.jpg',
     sub: <>Chairman</>,
-    linkedInLink: '',
-    githubLink: '',
+    linkedInLink: 'https://www.linkedin.com/in/sachin-mastud-670602206/?originalSubdomain=in',
   },
 ];
 
