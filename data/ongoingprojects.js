092ce814a77b7caeae54725ae57f5a5fb3c20405
Spnetic-5 <saurabhpowar1823@gmail.com>
2021-08-06 20:15:06 +0530

    added Eklavya in notifications

--- data/ongoingprojects.js
+++ data/ongoingprojects.js
@@ -2,10 +2,18 @@ const OngoingProjectsData = [
   {
     imgName: 'delta_bot.jpg',
     name: 'DELTA - PSP Bot',
-    sub:(
-        <>
-        The Team DNS from SRA bagged Second Prize in the 7th Delta International Automation Contest respresenting India globally.<br />
-        PSP Bot : Pick Sort and Place Bot is customly designed, 3 Axis XYZ Gantry based Pick and Place Robotic Solution for Industry Packaging lines housing variable sized input items or boxes. It uses and efficient item sorting and packaging algorithm to pick and place input items from an incoming conveyor into an output container ensuring minimum space wastage. Its primary use is in the  Multinational Packaging supply chain industry for variable sized item packaging and location based packaging.
+    sub: (
+      <>
+        The Team DNS from SRA bagged Second Prize in the 7th Delta International
+        Automation Contest respresenting India globally.
+        <br />
+        PSP Bot : Pick Sort and Place Bot is customly designed, 3 Axis XYZ
+        Gantry based Pick and Place Robotic Solution for Industry Packaging
+        lines housing variable sized input items or boxes. It uses and efficient
+        item sorting and packaging algorithm to pick and place input items from
+        an incoming conveyor into an output container ensuring minimum space
+        wastage. Its primary use is in the Multinational Packaging supply chain
+        industry for variable sized item packaging and location based packaging.
       </>
     ),
     githubLink: 'https://github.com/SRA-VJTI/Delta2021',
@@ -13,11 +21,7 @@ const OngoingProjectsData = [
   {
     imgName: 'smort.gif',
     name: 'SMORT',
-    sub:(
-        <>
-        Implementation of object tracking research papers.
-      </>
-    ),
+    sub: <>Implementation of object tracking research papers.</>,
     githubLink: 'https://github.com/saharshleo/OpenCV-Trackers',
   },
 ];
