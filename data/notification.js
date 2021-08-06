002cadc3dd4f28d57dacc90134823a22e2c8c51e
Spnetic-5 <saurabhpowar1823@gmail.com>
2021-08-06 20:15:06 +0530

    added Eklavya in notifications

--- data/notification.js
+++ data/notification.js
@@ -2,6 +2,7 @@ const NotificationData = [
   'March 21, 2021 - Seminar for First Years',
   'April 11, 2021 - Wall-E Worshop',
   'Mario Worshop for First Years',
+  'Eklavya Mentorship Program 2021'
 ];
 
 export default NotificationData;
