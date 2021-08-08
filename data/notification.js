c6b8d6ef6548cd6d6af48f1bf3b92b710053b339
Spnetic-5 <saurabhpowar1823@gmail.com>
2021-08-09 00:58:35 +0530

    made suggested changes

--- data/notification.js
+++ data/notification.js
@@ -1,8 +1,5 @@
 const NotificationData = [
-  'March 21, 2021 - Seminar for First Years',
-  'April 11, 2021 - Wall-E Worshop',
-  'Mario Worshop for First Years',
-  'Eklavya Mentorship Program 2021'
+  'Eklavya Mentorship Program 2021',
 ];
 
 export default NotificationData;
