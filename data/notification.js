a9c2e367a868ea2277742fd4f8d0017941d598cd
Saurabh Suresh Powar <sspowar_b19@el.vjti.ac.in>
2021-09-08 23:59:43 +0530

    added ongoing projects & minor changes

--- data/notification.js
+++ data/notification.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
 const NotificationData = ['Eklavya Mentorship Program 2021'];
-=======
-const NotificationData = [
-  'Eklavya Mentorship Program 2021 Selection rounds ongoing......',
-  'Eklavya Mentorship Program 2021 projects to be announced and to be opened for application soon',
-];
->>>>>>> 52e85ca4de9139bc38c4f074ae0260d3c9ef6c22
 
 export default NotificationData;
