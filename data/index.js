a54e6cea3ca08a2fc3fcb0ccb7ffbc39c6c83bcd
Saurabh Suresh Powar <66636289+Spnetic-5@users.noreply.github.com>
2021-10-07 11:55:49 +0530

    About VJTI, typo fixes, more ongoing projects (#25)
    * added Texas into sponsors
    * added Current team & Ongoing projects
    * added Eklavya in notifications
    * added ongoing projects & minor changes
    * added About VJTI

--- data/index.js
+++ data/index.js
@@ -1,6 +1,7 @@
 import {
   AboutUsAlumniText,
   AboutUsText,
+  AboutVJTI,
   AboutUsTeamText,
   AboutUsImages,
 } from './aboutus';
@@ -21,6 +22,7 @@ export {
   NavbarData,
   HeroTags,
   AboutUsText,
+  AboutVJTI,
   AboutUsTeamText,
   AboutUsAlumniText,
   AboutUsImages,
