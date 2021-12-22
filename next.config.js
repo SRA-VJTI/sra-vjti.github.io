ed34eeda12af913e68bd2af43c401559d2b5162e
Vedant Paranjape <22630228+VedantParanjape@users.noreply.github.com>
2021-12-22 19:20:04 +0530

    Fixed webpack loader config, had to put it inside the Transpiled modules

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
-const withTM = require('next-transpile-modules')(['emailjs-com']); // pass the modules you would like to see transpiled
+const withTM = require('next-transpile-modules')(['emailjs-com', 'raw-loader']); // pass the modules you would like to see transpiled
 
 
 module.exports = {
+<<<<<<< HEAD
   trailingSlash: true,
   ...withTM(),
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
@@ -13,3 +14,17 @@ module.exports = {
     return config;
   }
 };
+=======
+    trailingSlash: true, 
+    ...withTM({
+      webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        config.module.rules.push({
+          test: /\.md$/i,
+          loader: "raw-loader",
+        });
+    
+        return config;
+      }
+    }),
+    };
+>>>>>>> Fixed webpack loader config, had to put it inside the Transpiled modules
