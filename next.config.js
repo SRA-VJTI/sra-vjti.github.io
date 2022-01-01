728a7e2cafa9aafccb9de56c099183ab0d20219c
Vedant Paranjape <22630228+VedantParanjape@users.noreply.github.com>
2022-01-01 15:56:23 +0530

    Beautify code (suggested by the linter)

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 const withTM = require('next-transpile-modules')(['emailjs-com', 'raw-loader']); // pass the modules you would like to see transpiled
 
 module.exports = {
-    trailingSlash: true, 
-    ...withTM({
-      webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-        config.module.rules.push({
-          test: /\.md$/i,
-          loader: "raw-loader",
-        });
-    
-        return config;
-      }
-    }),
-    };
+  trailingSlash: true,
+  ...withTM({
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+      config.module.rules.push({
+        test: /\.md$/i,
+        loader: 'raw-loader',
+      });
+
+      return config;
+    },
+  }),
+};
