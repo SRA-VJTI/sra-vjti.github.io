0d0e526af7885b08301199b2a3a1c0d0903d1500
Hrishikesh-Bhagwat <79875904+Hrishikesh-Bhagwat@users.noreply.github.com>
2022-01-22 20:40:55 +0530

    Attempt to fix footer icon issue

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -8,30 +8,37 @@ import {
   faGithub,
   faLinkedinIn,
 } from '@fortawesome/free-brands-svg-icons';
+import { useEffect, useState } from 'react';
 import { SocialMediaLinks } from '../../data';
 
-const Footer = () => {
+function Footer(){
+  const [isLoad, setIsLoad] = useState('none');
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoad('');
+    }, 2000);
+  }, []);
   return (
     <>
       <footer className={styles.footer}>
         <h3>Connect with us</h3>
         <div className={styles.footerIcons}>
-          <a href={SocialMediaLinks.github} target='_blank'>
+          <a href={SocialMediaLinks.github} target='_blank' style={{ display: isLoad }}>
             <FontAwesomeIcon icon={faGithub} />
           </a>
-          <a href={SocialMediaLinks.linkedin} target='_blank'>
+          <a href={SocialMediaLinks.linkedin} target='_blank' style={{ display: isLoad }}>
             <FontAwesomeIcon icon={faLinkedinIn} />
           </a>
-          <a href={SocialMediaLinks.facebook} target='_blank'>
+          <a href={SocialMediaLinks.facebook} target='_blank' style={{ display: isLoad }}>
             <FontAwesomeIcon icon={faFacebook} />
           </a>
-          <a href={SocialMediaLinks.youtube} target='_blank'>
+          <a href={SocialMediaLinks.youtube} target='_blank' style={{ display: isLoad }}>
             <FontAwesomeIcon icon={faYoutube} />
           </a>
-          <a href={SocialMediaLinks.twitter} target='_blank'>
+          <a href={SocialMediaLinks.twitter} target='_blank' style={{ display: isLoad }}>
             <FontAwesomeIcon icon={faTwitter} />
           </a>
-          <a href={SocialMediaLinks.instagram} target='_blank'>
+          <a href={SocialMediaLinks.instagram} target='_blank' style={{ display: isLoad }}>
             <FontAwesomeIcon icon={faInstagram} />
           </a>
         </div>
