4e82e43e789e40fb11bd1cde8919238bf3e4b6fc
Sahethi <dgsahethi@gmail.com>
2022-02-21 21:07:33 +0530

    changed startup bg and linked the startups

--- data/startups.js
+++ data/startups.js
@@ -1,43 +1,20 @@
 const StartupsList = [
   {
-    name: 'Workshops',
+    name: 'Companies',
     startups: [
       {
         imgName: 'walle.jpg',
         name: 'Wall E',
         sub:
           'The Wall-E workshop focuses on the concepts of line-following and self-balancing, using ESP32, a powerful micro-controller with features like inbuilt Wi-Fi support and BLE, with extensive application in several domains like IoT, RF, etc. The workshop entails various essential concepts such as PID Control, Embedded Communication Protocols, PWM, Filters, RTOS, etc. and gives its attendees (first-year students) a general idea about the world of robotics and embedded hardware.',
-        githubLink: 'https://github.com/SRA-VJTI/Wall-E_v2.2',
-        youtubeLink: 'https://www.youtube.com/watch?v=-AbwiFBjHl0&t=36s',
+        externalLink: 'https://www.instamojo.com/',
       },
       {
         imgName: 'mario.jpg',
         name: 'Mario',
         sub:
           'In the MARIO workshop freshers design and implement a robotic manipulator(3-DOF Manipulator on ROS-based IO) using intricate concepts of forward and inverse kinematics, with Robot Operating System (ROS). ',
-        githubLink: 'https://github.com/SRA-VJTI/ROS-Workshop-2.1',
-        youtubeLink: 'https://www.youtube.com/watch?v=qkdTsRB-te4',
-      },
-    ],
-  },
-  {
-    name: 'Competitions',
-    startups: [
-      {
-        imgName: 'algocon.jpg',
-        name: 'Algocon',
-        sub:
-          'SRA conducts an online coding competition with problem statements on Data Structures and Algorithms chosen to exude the contestant’s sharp thinking.The top participants are chosen on the basis of the difficulty of the problem, and the efficacy of their solutions.',
-        githubLink: '',
-        youtubeLink: '',
-      },
-      {
-        imgName: 'labyrinth.png',
-        name: 'Labyrinth',
-        sub:
-          'The Labyrinth challenge is set to test the algorithm development skills of the participants. The objective  of this challenge is to identify and follow the shortest path in a given Black-and-white maze, using a wheeled bot to clear the course, all the while troubleshooting the hardware complications.  After the conclusion of Algocon, Labyrinth serves as the perfect challenge to showcase what the participants had learnt in the form of coding skills.',
-        githubLink: '',
-        youtubeLink: '',
+        externalLink: 'https://www.creto.club/',
       },
     ],
   },
