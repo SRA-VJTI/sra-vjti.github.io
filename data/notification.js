f6ac706d4d0c1c027f4dc63c213dfb0e69635e4f
Rishabh Bali <rishabhbali@pop-os.localdomain>
2022-03-12 21:11:04 +0530

    Wall-E 2.3 Notif

--- data/notification.js
+++ data/notification.js
@@ -1,5 +1,5 @@
 const NotificationData = [
-  "Vihaan Launch '22 : Introduction to SRA on 14th of January ",
+  "Wall-E 2.3 : Learn how to build a Line following and Self Balancing Bot from scratch on 16th March. Looking forward to see you there! ",
 ];
 
 export default NotificationData;
