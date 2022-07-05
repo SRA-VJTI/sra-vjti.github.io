524eedda77b1c9a424d7f3aa6cc54c3451b45c8c
Dhruv Kunjadiya <84779934+Dhruv454000@users.noreply.github.com>
2022-07-05 19:11:12 +0530

    Update notification.js

--- data/notification.js
+++ data/notification.js
@@ -1,5 +1,5 @@
 const NotificationData = [
-  'Eklavya-2k22 : Eklavya is a mentorship program conducted by Society of Robotics and Automation (SRA) where First Years will get an opportunity to work on full-fledged projects under the mentorship of SRA seniors. Looking forward towards your participation/seeing you there. ',
+  'Eklavya-2k22 : Eklavya is a mentorship program conducted by Society of Robotics and Automation (SRA) where First Years will get an opportunity to work on full-fledged projects under the mentorship of SRA seniors. Looking forward towards your participation. ',
 ];
 
 export default NotificationData;
