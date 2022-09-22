8d8d85908fdeb2b564966dd220d9dcf9d21ffccb
Dhruv454000 <dhruvkunjadiya55@gmail.com>
2022-09-22 22:42:12 +0530

    added blogs

---
+++ data/SRA-talks/2022/2022_shreyas_wasmedge.md
@@ -0,0 +1,19 @@
+# Introduction of Speaker ?
+ 
+Shreyas is a Final Year student at VJTI, Mumbai. He has been an embedded system enthusiast from the freshman year of college. He has been an open source contributor in GSoC in the year 2021 and 2022 in the organization BeagleBorad.org and Ste||ar group respectively. He was a CNCF and Linux Foundation Intern in which he had developed C-Extension for the Python SDK and benchmarks for it.
+ 
+# Gist of the talk?
+ 
+The talk introduced the attendees to The session ended with an interactive Q&A session. The attendees were then given further resources to explore by themselves.The session started with introduction of what was Wasmedge,what does WebAssembly mean and how does it enable a lightweight and high-performance for WebAssembly runtime.This SDK should be able to provide all the functions present in the C - API of WasmEdge.Moving on to the project details he introduced us to SDK which was used as reference and the workflow of the project.The session went on with further detailing of the implementation which was done with the aim of Wasmedge to be used as backend by providing an easy to use SDK over the latter ones.
+ 
+ 
+# More information on session.
+ 
+All of the information contained in the lecture can be found here: [Details of Project](https://satacker.github.io/WasmEdge/)
+ 
+ 
+# What is the best way to contact you ?
+ 
+You can contact me at my email, [Mail](shreyasatre16@gmail.com)
+ 
+Also you can connect with me on Linkedin, [Linkedin](https://linkedin.com/in/atreshreyas)
--- data/talks.js
+++ data/talks.js
@@ -27,7 +27,7 @@ const TalksList = [
         content: require('./SRA-talks/2022/2022_neel_himanshu_ml.md').default,
       },
       {
-        photo: '/static/images/blog/OmkarB.jpg',
+        photo: '/static/images/talks/omkar.jpeg',
         title: 'Session on GSOC experience with Beagleboard',
         author: 'Omkar Bhilare',
         content: require('./SRA-talks/2022/2022_omkar_beagleboard.md').default,
@@ -40,7 +40,7 @@ const TalksList = [
           .default,
       },
       {
-        photo: '/static/images/blog/Vedant.jpg',
+        photo: '/static/images/talks/vedant.png',
         title: 'Session on GSOC experience with Libcamera',
         author: 'Vedant Paranjape',
         content: require('./SRA-talks/2022/2022_vedant_libcamera.md').default,
@@ -50,6 +50,30 @@ const TalksList = [
         title: 'Talk on Leaf Classification(Image processing ML framework)',
         author: 'Abhinav Venkatadri',
         content: require('./SRA-talks/2022/2022_vedant_libcamera.md').default,
+      },
+      {
+        photo: '/static/images/talks/shreyas.png',
+        title: 'Session on experience with Wasmedge Organisation',
+        author: 'Shreyas Atre',
+        content: require('./SRA-talks/2022/2022_shreyas_wasmedge.md').default,
+      },
+      {
+        photo: '/static/images/talks/chinmay.png',
+        title: 'Session on PCB Design',
+        author: 'Chinmay Lonkar',
+        content: require('./SRA-talks/2022/2022_chinmay_pcb_design.md').default,
+      },
+      {
+        photo: '/static/images/talks/chinmay.png',
+        title: 'Session on BioAmp EXG',
+        author: 'Chinmay Lonkar, Moteen Shah',
+        content: require('./SRA-talks/2022/2022_chinmay_moteen_bioamp.md').default,
+      },
+      {
+        photo: '/static/images/talks/anushree_epfl.png',
+        title: 'Neuromechanical simulation of human locomotion',
+        author: 'Anushree Sabnis',
+        content: require('./SRA-talks/2022/2022_anushree_epfl.md').default,
       },
     ],
   },
