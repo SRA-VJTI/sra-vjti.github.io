fe69ad84acd6e0baa06afc672a468fe857d6ce89
Dhruv454000 <dhruvkunjadiya55@gmail.com>
2022-10-13 19:01:05 +0530

    updated images in current team

--- data/sponsors.js
+++ data/sponsors.js
@@ -1,9 +1,14 @@
-const SponsorListImages = ['espressif.png', 'texas.png', 'PCB_Power.jpeg','solidspace.png'];
+const SponsorListImages = [
+  'espressif.png',
+  'texas.png',
+  'PCB_Power.jpeg',
+  'solidspace.png',
+];
 const SponsorLinks = [
   'https://www.espressif.com/',
   'https://www.ti.com/',
   'https://www.pcbpower.com/',
-  'https://solidspace.co.in/'
+  'https://solidspace.co.in/',
 ];
 
 export { SponsorListImages, SponsorLinks };
