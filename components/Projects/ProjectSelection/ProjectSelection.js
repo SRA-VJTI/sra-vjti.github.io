c3f64e2265adbc3c69f4bc9858c312d2939d3435
Sameer Gupta <sameergupta4873@gmail.com>
2022-11-14 14:54:54 +0530

    updated css

--- components/Projects/ProjectSelection/ProjectSelection.js
+++ components/Projects/ProjectSelection/ProjectSelection.js
@@ -6,9 +6,15 @@ const ProjectSelection = () => {
     <div className={styles.projSelect}>
       <h1>Projects</h1>
       <div className={styles.projSelList}>
-        <Link href='/projects/flagship' className={styles.link}>Flagship Projects</Link>
-        <Link href='/projects/eklavya' className={styles.link}>Eklavya Projects</Link>
-        <Link href='/projects/ongoing' className={styles.link}>Ongoing Projects</Link>
+        <Link href='/projects/flagship' className={styles.link}>
+          Flagship Projects
+        </Link>
+        <Link href='/projects/eklavya' className={styles.link}>
+          Eklavya Projects
+        </Link>
+        <Link href='/projects/ongoing' className={styles.link}>
+          Ongoing Projects
+        </Link>
       </div>
     </div>
   );
