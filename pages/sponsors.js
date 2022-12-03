d6e8c71ec9cf6642e2a1a0ffff2194c863b120bd
Labib Asari <asari.r.labeeb7@gmail.com>
2022-12-03 15:42:10 +0530

    added title to sponsors page

--- pages/sponsors.js
+++ pages/sponsors.js
@@ -1,8 +1,12 @@
 import Sponsors from '../components/Sponsors/Sponsors';
+import Head from 'next/head';
 
 const SponsorsPage = () => {
   return (
     <>
+      <Head>
+        <title>Sponsors | SRA VJTI</title>
+      </Head>
       <Sponsors />
     </>
   );
