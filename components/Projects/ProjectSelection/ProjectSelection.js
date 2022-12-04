5c2ac7ee5701af23760880eaabd3f9bdb75730aa
Labib Asari <asari.r.labeeb7@gmail.com>
2022-12-04 17:49:33 +0530

    removed unnecessary fragments

--- components/Projects/ProjectSelection/ProjectSelection.js
+++ components/Projects/ProjectSelection/ProjectSelection.js
@@ -3,22 +3,20 @@ import Link from 'next/link';
 
 const ProjectSelection = () => {
   return (
-    <>
-      <div className={styles.projSelect}>
-        <h1>Projects</h1>
-        <div className={styles.projSelList}>
-          <Link href='/projects/flagship' className={styles.link}>
-            Flagship Projects
-          </Link>
-          <Link href='/projects/eklavya' className={styles.link}>
-            Eklavya Projects
-          </Link>
-          <Link href='/projects/ongoing' className={styles.link}>
-            Ongoing Projects
-          </Link>
-        </div>
+    <div className={styles.projSelect}>
+      <h1>Projects</h1>
+      <div className={styles.projSelList}>
+        <Link href='/projects/flagship' className={styles.link}>
+          Flagship Projects
+        </Link>
+        <Link href='/projects/eklavya' className={styles.link}>
+          Eklavya Projects
+        </Link>
+        <Link href='/projects/ongoing' className={styles.link}>
+          Ongoing Projects
+        </Link>
       </div>
-    </>
+    </div>
   );
 };
 
