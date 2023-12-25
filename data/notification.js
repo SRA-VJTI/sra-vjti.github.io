50eca942310a5c9942f1c34e6ab358b9b9c97a34
Mohd Alqama Shaikh <97826285+aPR0T0@users.noreply.github.com>
2023-12-25 20:00:25 +0530

    Update notification.js [25|12|2023] (#126)

--- data/notification.js
+++ data/notification.js
@@ -1,5 +1,8 @@
 const NotificationData = [
-  'Eklavya-2k22 : Eklavya is a mentorship program conducted by Society of Robotics and Automation (SRA) where First Years will get an opportunity to work on full-fledged projects under the mentorship of SRA seniors. ',
+  'Eklavya-2k23 : Eklavya projects can be found @ https://sravjti.in/projects/eklavya/',
+  'Team at SRA builds "Anuvadak" a pipe-crawling bot for Larsen & Turbo, that is aimed to provide communication to trapped workers in tunnel scenarios.',
+  'As the End semester exams are getting over and we are heading to the new year, SRA has some exciting news to share... So Stay tuned!',
+  'Internship Updates: 4 selections at EPFL for the Summer of 2023',
 ];
 
 export default NotificationData;
