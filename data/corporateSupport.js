486dcec7a49bbe12b268f5648a26356ff322cabb
Mohd Alqama Shaikh <97826285+aPR0T0@users.noreply.github.com>
2024-05-08 12:02:09 +0530

    [ Minor Changes ] : Adds Pics for CSR and Some Minor CSS Changes (#138)
    * [ Minor Updates ] : CSS Updates
    
    * [ Minor Additions ] : Multiple Pics addition for CSR

--- data/corporateSupport.js
+++ data/corporateSupport.js
@@ -8,29 +8,29 @@ const CsrPartners = [
     msg: 'We extend our heartfelt appreciation to GIC Re for their unwavering support through their CSR initiative. Their generosity has been instrumental in our mission to advance robotics, providing the crucial funds needed to modernize our laboratory with cutting-edge equipment and infrastructure.',
     changesMadeThroughCSR: [
       {
-        image: 'GIC_Electronics_Workbench.JPG',
+        image: 'GIC_Electronics_Workbench.jpg',
         Description:
           'An electronics workbench designed for conducting electronic activities ensures Electrostatic Discharge (ESD) safety, effectively preventing static charges.',
       },
       {
-        image: 'GIC_Mechanical_Workbench.JPG',
+        image: 'GIC_Mechanical_Workbench.jpg',
         Description:
           'A mechanical workbench, specifically designed for executing designated heavy mechanical work essential in robotics, is equipped with special provisions to safely secure the mechanical equipment.',
       },
       {
-        image: 'GIC_UltimakerS5.JPG',
+        image: 'GIC_UltimakerS5.jpg',
         Description:
           'The Ultimaker S5 pro, an advanced state-of-the-art 3D printer, brings imagination to life, particularly valuable for prototyping and producing high-quality industry-grade materials through 3D printing.',
       },
       {
-        image: 'GIC_High_Performance_Hardware.JPG',
+        image: 'GIC_High_Performance_Hardware.jpg',
         Description:
           'Utilizing cutting-edge computer hardware enables the execution of complex calculations necessary for implementation, coupled with robust support for resource-intensive CAD software.',
       },
     ],
     facilities: [
       {
-        image: 'GIC_Inauguration.JPG',
+        image: 'GIC_Inauguration.jpg',
         Description:
           'The SRA Lab was inaugurated by Shri. Mukesh Waghela, Deputy General Manager at GIC Re, on May 3rd, 2024, in the presence of Dr. Sachin Kore, Director of VJTI.',
       },
@@ -40,7 +40,7 @@ const CsrPartners = [
           'The ribbon cutting ceremony was performed by Smt. Sneha Nair, Assistant General Manager at GIC Re, on May 3rd, 2024.',
       },
       {
-        image: 'GIC_Momento_DGM.JPG',
+        image: 'GIC_Momento_DGM.jpg',
         Description:
           'Shri. Mukesh Waghela, Deputy General Manager at GIC Re (on the left), is being honored by Dr. Sachin Kore, Director of VJTI (on the right), for his support in the development of the SRA Lab.',
       },
@@ -50,7 +50,7 @@ const CsrPartners = [
           'We extend our gratitude to the entire team for their belief in us and their assistance in the development of the SRA Lab at VJTI.',
       },
       {
-        image: 'GIC_SRA_LAB.JPG',
+        image: 'GIC_SRA_LAB.jpg',
         Description:
           'Newly developed state of the art SRA Lab, under the CSR initiative of GIC Re.',
       },
@@ -60,7 +60,7 @@ const CsrPartners = [
           'The SRA Research & Development Hub: A newly constructed mezzanine floor facility as part of the CSR initiative by GIC Re.',
       },
       {
-        image: 'GIC_SRA_Hall_Fame.JPG',
+        image: 'GIC_SRA_Hall_Fame.jpg',
         Description:
           'Wall of Fame: Significant achievements of SRA commemorated on the wall.',
       },
