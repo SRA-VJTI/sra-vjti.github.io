03c319dcd2782baf0ec0b4e19fd82e188e129146
Saish Karole <72208314+NachtSpyder04@users.noreply.github.com>
2024-09-13 23:06:56 +0530

    add new ongoing projects and update UI for the same page (#6)

--- components/Projects/OngoingProjects/OngoingProjects.js
+++ components/Projects/OngoingProjects/OngoingProjects.js
@@ -1,11 +1,14 @@
 import styles from './OngoingProjects.module.scss';
 import Link from 'next/link';
+import React, { useState } from 'react';
 import { OngoingProjectsData } from '../../../data';
 import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 import Hero from '../../Hero/Hero';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const OngoingProjects = () => {
+  const [selectedProject, setSelectedProject] = useState(OngoingProjectsData[0])
+
   return (
     <>
       <Hero
@@ -14,41 +17,68 @@ const OngoingProjects = () => {
         subtitleList={['Ingenuinity in progress.']}
         isHome={false}
       />
-      <div className={styles.ongoingProjects} id='is'>
-        {OngoingProjectsData.map((proj, idx) => {
-          return (
-            <OngoingProjectCard
-              key={`ongoing_project_${idx}`}
-              imgName={proj.imgName}
-              name={proj.name}
-              sub={proj.sub}
-              githubLink={proj.githubLink}
-            />
-          );
-        })}
+      <div className={styles.projectSelector}>
+        {OngoingProjectsData.map((proj, idx) => (
+          <button
+            key={idx}
+            className={`${styles.projectName} ${proj === selectedProject ? styles.active : ''}`}
+            onClick={() => setSelectedProject(proj)}
+          >
+            {proj.name}
+          </button>
+        ))}
+      </div>
+      <div className={styles.ongoingProjects}>
+        <OngoingProjectCard
+          key={selectedProject.name}
+          imgName={selectedProject.imgName}
+          name={selectedProject.name}
+          sub={selectedProject.sub}
+          githubLink={selectedProject.githubLink}
+          modelLink={selectedProject.modelLink}
+        />
       </div>
     </>
-  );
-};
+  )
+}
 
-const OngoingProjectCard = ({ imgName, name, sub, githubLink }) => {
+const OngoingProjectCard = ({ imgName, name, sub, githubLink, modelLink }) => {
   return (
     <div className={styles.ongoingProj}>
-      <div
-        style={{ backgroundImage: `url("/static/images/${imgName}")` }}
-        className={styles.ongoingProjImg}
-      ></div>
       <div className={styles.ongoingProjCont}>
         <div className={styles.ongoingProjContName}>{name}</div>
         <div className={styles.ongoingProjContSub}>{sub}</div>
         <div className={styles.ongoingProjContLinks}>
-          <a href={githubLink}>
+          <a href={githubLink} target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faGithub} />
           </a>
         </div>
       </div>
-    </div>
-  );
-};
+      {imgName && ( <div
+        className={styles.ongoingProjImg}
+        style={{
+          backgroundImage: `url("/static/images/${imgName}")`,
+          backgroundPosition: 'center',
+          backgroundSize: 'contain', // Adjust if needed (can use 'cover' too)
+          backgroundRepeat: 'no-repeat',
+          height: '400px', // Ensure the container has a fixed height
+          width: '45%', // Ensure the image takes up a certain width of the container
+        }}
 
+      ></div>)}
+      {modelLink && (
+        <div style={{ paddingRight: 50}}>
+        <iframe 
+          allowfullscreen  
+          width="640"
+          height="480" 
+          loading="lazy" 
+          frameborder="1" 
+          src={modelLink}>
+            </iframe>
+        </div>)}
+      
+    </div>
+  )
+}
 export default OngoingProjects;
