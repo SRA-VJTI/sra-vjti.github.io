3038adc07163552a949b7d60b530855533ef18b0
Varun Patil <vap05072006@gmail.com>
2026-02-18 19:41:23 +0530

    Updated Alumnis

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -2,9 +2,21 @@ import styles from './Card.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 
-const Card = ({ index, imgName, name, sub, linkedInLink, githubLink }) => {
+const Card = ({
+  index,
+  imgName,
+  name,
+  sub,
+  linkedInLink,
+  githubLink,
+  isAlumni,
+  previous,
+  current,
+}) => {
+  const hasStatusData = isAlumni && (previous || current);
+
   return (
-    <div className={styles.cont}>
+    <div className={`${styles.cont} ${isAlumni ? styles.alumni : ''}`}>
       <div className={styles.top}>
         <div
           className={styles.personImg}
@@ -13,7 +25,42 @@ const Card = ({ index, imgName, name, sub, linkedInLink, githubLink }) => {
       </div>
       <div className={styles.txtCont}>
         <div className={styles.name}>{name}</div>
-        <div className={styles.desc}>{sub}</div>
+        {hasStatusData ? (
+          <div className={styles.statusContent}>
+            {current && current.length > 0 && (
+              <div className={styles.statusSection}>
+                <div className={styles.statusLabel}>Now</div>
+                <div className={styles.pills}>
+                  {current.map((item, i) => (
+                    <span
+                      key={`c-${i}`}
+                      className={`${styles.pill} ${styles.highlight}`}
+                    >
+                      {item.t}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            )}
+            {previous && previous.length > 0 && (
+              <div className={`${styles.statusSection} ${styles.hoverOnlySection}`}>
+                <div className={styles.statusLabel}>Previously</div>
+                <div className={styles.pills}>
+                  {previous.map((item, i) => (
+                    <span
+                      key={`p-${i}`}
+                      className={`${styles.pill} ${styles.highlight}`}
+                    >
+                      {item.t}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            )}
+          </div>
+        ) : (
+          <div className={styles.desc}>{sub}</div>
+        )}
         <div className={styles.icons}>
           <GitHubLink githubLink={githubLink} />
           <LinkedInLink linkedInLink={linkedInLink} />
